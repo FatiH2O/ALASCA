12643df0ffd6e71a2eef8d96036d80beb0167481
Hiba <116457271+hibaoqba@users.noreply.github.com>
2025-11-12 23:37:36 +0100

    Delete hem-2025-e1/fr/sorbonne_u/components/hem2025e1/equipments/simpleCharger directory

--- hem-2025-e1/fr/sorbonne_u/components/hem2025e1/equipments/simpleCharger/ChargerCI.java
+++
@@ -1,6 +0,0 @@
-package fr.sorbonne_u.components.hem2025e1.equipments.simpleCharger;
-
-import fr.sorbonne_u.components.interfaces.OfferedCI;
-import fr.sorbonne_u.components.interfaces.RequiredCI;
-
-public interface ChargerCI extends OfferedCI, RequiredCI, ChargerI {}
